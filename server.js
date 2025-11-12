f20b19484e7cb04ddc9c531ca34432b5c23a107a
cmhanner <chanceforcode@gmail.com>
2025-11-12 17:06:52 -0500

    updating for class

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require ("express");
 const cors = require("cors");
 const multer = require("multer");
 const app = express();
+const Joi = require("joi") //  have to install
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
